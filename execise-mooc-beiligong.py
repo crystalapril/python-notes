8be39a9ea6bd94c14c3a134d65b4e82dfb8bb51a
crystalapril <aprilzhangpku@qq.com>
2019-06-19 17:08:45 +0800

    Update execise-mooc-beiligong.py

--- execise-mooc-beiligong.py
+++ execise-mooc-beiligong.py
@@ -245,7 +245,71 @@ n=getNum()
 m=mean(n)
 print("平均值：{}，方差：{:.2}，中位数：{}.".format(m,dev(n,m),median(n)))
  
-#6.2    
+#6.2 hamlet英文词频统计
+def getText():
+    txt=open("hamlet.txt","r").read()
+    txt=txt.lower()
+    for ch in '!"#$%&*+,-./:;<=>?@[\\]^_＇{|}"':
+        txt = txt.replace(ch," ")
+    return txt
+    
+hamletTxt = getText()
+words = hamletTxt.split()
+counts = {}
+for word in words:
+    counts[word] = counts.get(word,0)+1
+items = list(counts.items())
+items.sort(key=lambda x :x[1],reverse= True)
+for i in range(10):
+    word,count = items[i]
+    print("{0:<10}{1:>5}".format(word,count))
+    
+#6.3 三国演义人物出场统计  中文分词，使用jieba
+#6.3.1
+import jieba
+txt = open("threekingdoms.txt","r",encoding="utf-8").read()
+words = jieba.lcut(txt)
+counts={}
+for word in words:
+    if len(word) == 1:
+        continue
+    else:
+        counts[word]=counts.get(word,0)+1
+items = list(counts.items())
+items.sort(key=lambda x :x[1],reverse= True)
+for i in range(10):
+    word,count = items[i]
+    print("{0:<10}{1:>5}".format(word,count))  
+    
+#6.3.2
+import jieba
+txt = open("threekingdoms.txt","r",encoding="utf-8").read()
+excludes = {"将军","却说","荆州","二人","不可","不能","如此","商议","如何","军士"}
+words = jieba.lcut(txt)
+counts={}
+for word in words:
+    if len(word) == 1:
+        continue
+    elif word == "诸葛亮" or word == "孔明曰"：
+        rword = "孔明"
+    elif word == "关公" or word == "云长"：
+        rword = "关羽"
+    elif word == "玄德" or word == "玄德曰"：
+        rword = "刘备"
+    elif word == "孟德" or word == "丞相曰"：
+        rword = "曹操"
+    else:
+        reword = word
+    counts[rword]=counts.get(rword,0)+1
+for word in excludes:
+    del counts[word]
+items = list(counts.items())
+items.sort(key=lambda x :x[1],reverse= True)
+for i in range(10):
+    word,count = items[i]
+    print("{0:<10}{1:>5}".format(word,count))  
+    
+    
 
 
 
