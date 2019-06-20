46a376758108203acf7fdcc5ad404358590dd4db
Roc King <ownwaterloo@gmail.com>
2019-06-20 12:04:48 +0800

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,21 @@
+# https://en.wikipedia.org/wiki/Conversion_of_units_of_temperature#Celsius_(centigrade)
+# https://en.wikipedia.org/wiki/Celsius
+# https://en.wikipedia.org/wiki/Kelvin
+# https://en.wikipedia.org/wiki/Fahrenheit
+
+def celsius_to_kelvin(xs):
+  pass
+
+def kelvin_to_celsius(xs):
+  pass
+
+def celsius_to_fahrenheit(xs):
+  pass
+
+def fahrenheit_to_celsius(xs):
+  pass
+
+celsius_to_kelvin([0, 1, 2]) == [273.15, 274.15, 275.15]
+kelvin_to_celsius([0, 1, 2]) == [-273.15, -272.15, -271.15]
+celsius_to_fahrenheit([0, 5, 10]) == [32, 41, 50]
+fahrenheit_to_celsius([32, 41, 50]) == [0, 5, 10]
