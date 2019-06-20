863f3191e6d72d6158bbef2d6755441cd3f2f20a
crystalapril <aprilzhangpku@qq.com>
2019-06-20 22:18:31 +0800

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -4,16 +4,44 @@
 # https://en.wikipedia.org/wiki/Fahrenheit
 
 def celsius_to_kelvin(xs):
+    l=[]
+    for x in xs:
+        l.append(x+273.15)      
+    return l
+
+def celsius_to_kelvin(xs):
     return list(map(lambda x:x+273.15,xs))
+
+
+def kelvin_to_celsius(xs):
+    l=[]
+    for x in xs:
+        l.append(x-273.15)  
+    return l
 
 def kelvin_to_celsius(xs):
     return list(map(lambda x:x-273.15,xs))
 
+
+def celsius_to_fahrenheit(xs):
+    l=[]
+    for x in xs:
+        l.append(x*1.8+32)      
+    return l
+
 def celsius_to_fahrenheit(xs):
     return list(map(lambda x:x*1.8+32,xs))
 
+
+def fahrenheit_to_celsius(xs):
+    l=[]
+    for x in xs:
+        l.append((x-32)/1.8)      
+    return l
+
 def fahrenheit_to_celsius(xs):
     return list(map(lambda x:(x-32)/1.8,xs))
+
 
 celsius_to_kelvin([0, 1, 2]) == [273.15, 274.15, 275.15]
 kelvin_to_celsius([0, 1, 2]) == [-273.15, -272.15, -271.15]
