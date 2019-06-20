82698dd8af1fc21976849f0f15e6e7d14107cc66
Roc King <ownwaterloo@gmail.com>
2019-06-20 12:24:14 +0800

    Create melting_point.py

---
+++ melting_point.py
@@ -0,0 +1,39 @@
+# https://en.wikipedia.org/wiki/Melting_point
+# https://en.wikipedia.org/wiki/Triple_point
+# https://en.wikipedia.org/wiki/List_of_chemical_elements
+
+melting_point = [
+  ('C'  , 3800   ),
+  ('Al' ,  933.47),
+  ('Fe' , 1811   ),
+  ('Cu' , 1357.77),
+  # ('Ag' , 1234.93),
+  ('W'  , 3695   ),
+  ('Pt' , 2041.4 ),
+  # ('Au' , 1337.33),
+  # ('Hg' ,  234.43),
+]
+
+def get_symbol(xs):
+  pass
+
+def get_melting_point(xs):
+  pass
+
+get_symbol       (melting_point) == ['C' , 'Al'  , 'Fe', 'Cu'   , 'W' , 'Pt'  ]
+get_melting_point(melting_point) == [3800, 933.47, 1811, 1357.77, 3695, 2041.4]
+
+def to_celsius(xs):
+  pass
+
+to_celsius(melting_point) == [
+  ('C' , 3526.85),
+  ('Al',  660.32),
+  ('Fe', 1537.85),
+  ('Cu', 1084.62),
+  # ('Ag',  961.78),
+  ('W',  3421.85),
+  ('Pt', 1768.25),
+  # ('Au', 1064.18),
+  # ('Hg',  -38.72),
+]
