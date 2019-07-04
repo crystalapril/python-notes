834bfcbaf8aa994e0f6fbf900b99a3692f50311f
crystalapril <aprilzhangpku@qq.com>
2019-07-04 21:50:09 +0800

    Update search.py

--- search.py
+++ search.py
@@ -26,14 +26,22 @@ sfw = [
 ]
 
 def search_by_serial(books, serial):
-  pass
+    a=dict()
+    for x in books:
+        if x['serial']==serial:
+            a=x
+    return a
 
 search_by_serial(sfw, 381) == {'date': '2018-02', 'serial': 381, 'other': '...'}
 search_by_serial(sfw, 368) == {'date': '2017-01', 'serial': 368, 'other': '...'}
 search_by_serial(sfw, 800) == None
 
 def search_by_date(books, date):
-  pass
+    a=dict()
+    for x in books:
+        if x['date']==date:
+            a=x
+    return a
 
 search_by_date(sfw, '2018-02') == {'date': '2018-02', 'serial': 381, 'other': '...'}
 search_by_date(sfw, '2017-01') == {'date': '2017-01', 'serial': 368, 'other': '...'}
