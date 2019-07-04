2c301994889d5652b7f099010c059a4c20c76206
Roc King <ownwaterloo@gmail.com>
2019-07-04 17:26:58 +0800

    reindent

--- search.py
+++ search.py
@@ -9,20 +9,20 @@
 #  for x in range(14)]
 
 sfw = [
-    {'date': '2018-02', 'serial': 381, 'other': '...'},
-    {'date': '2018-01', 'serial': 380, 'other': '...'},
-    {'date': '2017-12', 'serial': 379, 'other': '...'},
-    {'date': '2017-11', 'serial': 378, 'other': '...'},
-    {'date': '2017-10', 'serial': 377, 'other': '...'},
-    {'date': '2017-09', 'serial': 376, 'other': '...'},
-    {'date': '2017-08', 'serial': 375, 'other': '...'},
-    {'date': '2017-07', 'serial': 374, 'other': '...'},
-    {'date': '2017-06', 'serial': 373, 'other': '...'},
-    {'date': '2017-05', 'serial': 372, 'other': '...'},
-    {'date': '2017-04', 'serial': 371, 'other': '...'},
-    {'date': '2017-03', 'serial': 370, 'other': '...'},
-    {'date': '2017-02', 'serial': 369, 'other': '...'},
-    {'date': '2017-01', 'serial': 368, 'other': '...'},
+  {'date': '2018-02', 'serial': 381, 'other': '...'},
+  {'date': '2018-01', 'serial': 380, 'other': '...'},
+  {'date': '2017-12', 'serial': 379, 'other': '...'},
+  {'date': '2017-11', 'serial': 378, 'other': '...'},
+  {'date': '2017-10', 'serial': 377, 'other': '...'},
+  {'date': '2017-09', 'serial': 376, 'other': '...'},
+  {'date': '2017-08', 'serial': 375, 'other': '...'},
+  {'date': '2017-07', 'serial': 374, 'other': '...'},
+  {'date': '2017-06', 'serial': 373, 'other': '...'},
+  {'date': '2017-05', 'serial': 372, 'other': '...'},
+  {'date': '2017-04', 'serial': 371, 'other': '...'},
+  {'date': '2017-03', 'serial': 370, 'other': '...'},
+  {'date': '2017-02', 'serial': 369, 'other': '...'},
+  {'date': '2017-01', 'serial': 368, 'other': '...'},
 ]
 
 def search_by_serial(books, serial):
